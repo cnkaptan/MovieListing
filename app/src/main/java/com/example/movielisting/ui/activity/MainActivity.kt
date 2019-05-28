608178f48da808b5385023b0ebd84bdac790772d
Cihan Kaptan <cihan-kaptan@outlook.com>
2019-05-28 14:49:20 +0300

    Tablets are supported

--- app/src/main/java/com/example/movielisting/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/movielisting/ui/activity/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.movielisting.ui.activity
 
+import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.FrameLayout
+import androidx.annotation.Nullable
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
 import butterknife.BindView
 import butterknife.ButterKnife
 import com.example.movielisting.R
@@ -14,11 +19,19 @@ import javax.inject.Inject
 
 class MainActivity : AppCompatActivity(), HasSupportFragmentInjector {
 
+    @Nullable
+    @BindView(R.id.detail_container)
+    lateinit var detailContainer: FrameLayout
+
     @Inject
     lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Fragment>
 
     override fun supportFragmentInjector(): DispatchingAndroidInjector<Fragment> {
         return dispatchingAndroidInjector
+    }
+
+    private val isLargeScreen: Boolean by lazy {
+        resources.getBoolean(R.bool.is_large_screen)
     }
 
 
@@ -27,18 +40,28 @@ class MainActivity : AppCompatActivity(), HasSupportFragmentInjector {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         ButterKnife.bind(this)
+        if (isLargeScreen) {
+            requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        }
 
-        supportFragmentManager.beginTransaction()
-            .add(R.id.fragment_container, MovieListFragment())
-            .commit()
+        supportFragmentManager.commit {
+            add(R.id.fragment_container, MovieListFragment())
+        }
     }
 
 
     fun openDetails(movie: MovieEntity) {
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragment_container, MovieDetailsFragment.newInstance(movie))
-            .addToBackStack(null)
-            .commit()
+        if (isLargeScreen) {
+            supportFragmentManager.commit {
+                add(R.id.detail_container, MovieDetailsFragment.newInstance(movie))
+            }
+            detailContainer.visibility = View.VISIBLE
+            return
+        }
+        supportFragmentManager.commit {
+            replace(R.id.fragment_container, MovieDetailsFragment.newInstance(movie))
+            addToBackStack(null)
+        }
     }
 
     override fun onBackPressed() {
